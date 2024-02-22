c76b81ba5afe0a25ac42a4c3bf00d33ea3861384
ermarin <marinerick08@gmail.com>
2024-02-22 14:02:17 -0600

    :lipstick: Add styles

--- src/screens/Home.js
+++ src/screens/Home.js
@@ -1,8 +1,36 @@
 import React from 'react';
-import { View } from 'react-native';
+import { SafeAreaView, StyleSheet, View } from 'react-native';
+import { Button, Text } from '@chakra-ui/react';
 
 export default function Home({ navigation, route }) {
+  const exit = () => {
+    navigation.navigate('Login');
+  }
+
   return (
-    <View>HOLA HOME</View>
+    <SafeAreaView style={styles.container}>
+      <View>
+        <Text
+          bgGradient='linear(to-l, #7928CA, #FF5733)'
+          bgClip='text'
+          fontSize='6xl'
+          fontWeight='extrabold'
+        >
+          GRACIAS POR TÚ VISITA...
+        </Text>
+      </View>
+      <View style={styles.contBtn}>
+        <Button variant='link' onClick={exit}>Salir</Button>
+      </View>
+    </SafeAreaView>
   )
 }
+
+const styles = StyleSheet.create({
+  container: {
+    margin: 40,
+  },
+  contBtn: {
+    marginTop: 200,
+  },
+});
--- src/screens/Login.js
+++ src/screens/Login.js
@@ -1,8 +1,69 @@
-import React from 'react';
-import { View } from 'react-native';
+import React, { useState } from 'react';
+import { SafeAreaView, StyleSheet, View } from 'react-native';
+import {
+  Button,
+  Input,
+  InputGroup,
+  InputLeftElement,
+  InputRightElement,
+  Text,
+} from '@chakra-ui/react';
+import { EmailIcon, LockIcon, ViewIcon, ViewOffIcon } from '@chakra-ui/icons';
 
 export default function Login({ navigation, route }) {
+  const [show, setShow] = useState(false);
+  const handleClick = () => setShow(!show);
+  const valUser = () => {
+    navigation.navigate('Home');
+  }
+
   return (
-    <View>HOLA Login</View>
+    <SafeAreaView style={styles.container}>
+      <View style={styles.conts}>
+        <Text fontSize='3xl' fontWeight='bold'>Cuidado con el perro</Text>
+      </View>
+      <View style={styles.conts}>
+        <InputGroup styles={{ marginVertical: 10 }}>
+          <InputLeftElement pointerEvents='none'>
+            <EmailIcon color='gray.500' />
+          </InputLeftElement>
+          <Input type='email' placeholder='Email' variant='flushed' />
+        </InputGroup>
+      </View>
+
+      <View>
+        <InputGroup>
+          <InputLeftElement pointerEvents='none'>
+            <LockIcon color='gray.500' />
+          </InputLeftElement>
+          <Input type={show ? 'text' : 'password'} placeholder='Password' variant='flushed' />
+          <InputRightElement>
+            <Button onClick={handleClick}>
+              {show ? <ViewOffIcon /> : <ViewIcon />}
+            </Button>
+          </InputRightElement>
+        </InputGroup>
+      </View>
+
+      <View style={styles.conts}>
+        <Button onClick={valUser}>Entrar</Button>
+      </View>
+      <View style={styles.conts}>
+        <Button variant='link'>
+          No tienes una cuenta? Registrate!
+        </Button>
+      </View>
+    </SafeAreaView>
   )
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    flexDirection: 'column',
+    marginHorizontal: 50,
+  },
+  conts: {
+    paddingVertical: 20,
+  },
+});
