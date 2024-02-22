3facd289cf4ad61bd05257e04ddb26c35ad281f2
ermarin <marinerick08@gmail.com>
2024-02-22 14:02:17 -0600

    :lipstick: Add styles

--- src/App.js
+++ src/App.js
@@ -1,6 +1,9 @@
-import React, { useState } from 'react';
+import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
+
+import { ChakraProvider } from '@chakra-ui/react';
+
 import Home from './screens/Home';
 import Login from './screens/Login';
 
@@ -9,33 +12,24 @@ const Stack = createNativeStackNavigator();
 export default function App() {
 
   return (
-    <NavigationContainer>
-      <Stack.Navigator
-        initialRouteName={'Login'}
-        screenOptions={{
-          headerTitleAlign: 'center',
-          headerStyle: {
-            backgroundColor: '#0080ff',
-          },
-          headerTintColor: '#ffffff',
-          headerTitleStyle: {
-            fontSize: 25,
-            fontWeight: 'bold',
-          },
-        }}
-      >
-        <Stack.Screen
-          name={'Login'}
-          component={Login}
-          options={{
-            headerShown: false,
-          }}
-        />
-        <Stack.Screen
-          name={'Home'}
-          component={Home}
-        />
-      </Stack.Navigator>
-    </NavigationContainer>
+    <ChakraProvider>
+      <NavigationContainer>
+        <Stack.Navigator
+          initialRouteName={'Login'}
+        >
+          <Stack.Screen
+            name={'Login'}
+            component={Login}
+            options={{
+              headerShown: false,
+            }}
+          />
+          <Stack.Screen
+            name={'Home'}
+            component={Home}
+          />
+        </Stack.Navigator>
+      </NavigationContainer>
+    </ChakraProvider>
   );
 }
